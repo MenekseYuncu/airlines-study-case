51c3c4e59d61d98dddd6d255096248d4b1332283
MenekseYuncu <menekseyuncu@gmail.com>
2025-09-10 14:59:29 +0300

    initial commit

---
+++ src/main/java/com/menekse/airlines/AirlinesStudyCaseApplication.java
@@ -0,0 +1,13 @@
+package com.menekse.airlines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlinesStudyCaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirlinesStudyCaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/menekse/airlines/AirlinesStudyCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.menekse.airlines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlinesStudyCaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
